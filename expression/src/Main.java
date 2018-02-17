462202e2a31f001f170d10de9ae0e2cb0e4632f3
unknown <leeoyan@hotmail.com>
2018-02-17 11:26:44 +0800

    expression

---
+++ expression/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.math.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Scanner input = new Scanner(System.in);
+	    String exp = input.nextLine();
+	    Stack<String> op = new Stack<>();
+	    Stack<String> num = new Stack<>();
+	    for (int i = 0; i < exp.length(); i++) {
+	        
+        }
+    }
+}
