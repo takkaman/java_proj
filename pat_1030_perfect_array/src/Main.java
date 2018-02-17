6386fb015daf81e0cb05e16646a853231abd3b1a
unknown <leeoyan@hotmail.com>
2018-02-17 11:27:33 +0800

    pat 1030

---
+++ pat_1030_perfect_array/src/Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String line = input.nextLine();
+        int ttlNum = Integer.valueOf(line.split(" ")[0]);
+        int p =  Integer.valueOf(line.split(" ")[1]);
+        String numArr = input.nextLine();
+        
+    }
+}
