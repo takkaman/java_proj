c8e139de0d4c813725f78646ede6caa1ad8dd67e
unknown <leeoyan@hotmail.com>
2018-02-17 21:15:11 +0800

    update

--- pat_1030_perfect_array/src/Main.java
+++ pat_1030_perfect_array/src/Main.java
@@ -3,11 +3,26 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+        int start=0, end=0;
         Scanner input = new Scanner(System.in);
         String line = input.nextLine();
         int ttlNum = Integer.valueOf(line.split(" ")[0]);
-        int p =  Integer.valueOf(line.split(" ")[1]);
-        String numArr = input.nextLine();
-        
+        Double p =  Double.valueOf(line.split(" ")[1]);
+        String numArr_tmp = input.nextLine();
+        Double[] numArr = new Double[ttlNum];
+        String [] numArrS = numArr_tmp.split(" ");
+        for (int i = 0; i < ttlNum; i++) {
+            numArr[i] = Double.valueOf(numArrS[i]);
+        }
+        Arrays.sort(numArr);
+        while(end<ttlNum) {
+//            System.out.println(numArr[start] +" "+ numArr[end]);
+            if (numArr[end] <= p*numArr[start]) {
+                end++;
+            } else {
+                start++;
+            }
+        }
+        System.out.println(end-start);
     }
 }
