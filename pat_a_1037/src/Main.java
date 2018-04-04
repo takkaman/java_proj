68e661e12493b7080ca28070bc4feadcb1a2e17a
Leo Yan <phyan@internal.synopsys.com>
2018-04-04 11:05:25 +0800

    update

---
+++ pat_a_1037/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Main{
+	public static long GetMaxVal(int[] coup, int[] prod) {
+		long val = 0;
+		int i=0, j=0;
+		Arrays.sort(coup);
+//		System.out.println(Arrays.toString(coup));
+		Arrays.sort(prod);
+//		System.out.println(Arrays.toString(prod));
+		while (coup[i] < 0 && prod[j] < 0) {
+			val += coup[i++] * prod[j++];
+		}
+		
+		i = coup.length - 1;
+		j = prod.length - 1;
+		while (coup[i] > 0 && prod[j] > 0) {
+			val += coup[i--] * prod[j--];
+		}
+		
+		return val;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i;
+		int nc = sc.nextInt();
+		int[] coup = new int[nc];
+		for (i = 0; i < nc; i++) {
+			coup[i] =sc.nextInt();
+		}
+		int np = sc.nextInt();
+		int[] prod = new int[np];
+		for (i = 0; i < np; i++) {
+			prod[i] = sc.nextInt();
+		}
+		sc.close();
+		System.out.println(GetMaxVal(coup, prod));
+	}
+}
