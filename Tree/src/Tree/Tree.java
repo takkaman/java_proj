4234192492c10e8101e3f1f154ee0ac3e5b18659
unknown <leeoyan@hotmail.com>
2018-04-24 23:07:38 +0800

    reconstruct via pre and in finished

--- Tree/src/Tree/Tree.java
+++ Tree/src/Tree/Tree.java
@@ -53,6 +53,19 @@ public class Tree {
         return sum;
     }
 
+    public TreeNode reConstructBinaryTree(int [] pre,int [] in) {
+        if (pre.length == 0) return null;
+        TreeNode root = new TreeNode(pre[0]);
+        int i;
+        for (i = 0; i < in.length; i++) {
+            if (in[i] == pre[0]) break;
+        }
+        root.left = reConstructBinaryTree(Arrays.copyOfRange(pre, 1, i+1), Arrays.copyOfRange(in, 0, i));
+        root.right = reConstructBinaryTree(Arrays.copyOfRange(pre, 1+i, pre.length), Arrays.copyOfRange(in, i+1, in.length));
+
+        return root;
+    }
+    
     public static void PreOrder(TreeNode root) {
         if (root == null) return;
         System.out.println(root.key);
